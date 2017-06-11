1371fdec6aa4e9991ec4103868b200f5c4cf0b1f
Rduous <git@rduous.e4ward.com>
2017-06-11 07:01:53 -0600

    Added onReady and navbar behavior to main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,22 +1,31 @@
-// Carousel logic
-$('.carousel').slick({
-  arrows: false,
-  autoplay: true,
-  autoplaySpeed: 4000,
-  draggable: false,
-  pauseOnFocus: false,
-  pauseOnHover: false
+$(document).ready(function() {
+
+  // Carousel logic
+  $('.carousel').slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+
+  // Navbar/scroll behavior
+  var verticalScroll = 0;
+
+  $(window).scroll(function() {
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop - 50 > verticalScroll) {
+      var navBarHeight = $('.navbar').css('height');
+      $('.navbar').animate({
+        top: '-' + navBarHeight
+      }, 150);
+    } else if (verticalScroll - 50 > scrollTop) {
+      $('.navbar').animate({
+        top: 0
+      }, 150);
+    }
+    verticalScroll = scrollTop;
+  });
+
 });
-
-// Navbar/scroll behavior
-var verticalScroll = 0;
-
-// $(window).scroll(function() {
-//   var scrollTop = $(this).scrollTop();
-//   if (scrollTop - 50 >= verticalScroll) {
-//     var navBarHeight = $('.navbar').outerHeight();
-//     $('.navbar').animate({
-//       height: - navBarHeight
-//     },150);
-//   }
-// });
